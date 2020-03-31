4392657e8af72847000988919575c3702ba854f5
nsakib <nat.sakib@gmail.com>
2020-03-31 18:43:36 -0400

    Employee test set functions working

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,26 @@
 // TODO: Write code to define and export the Employee class
+
+function Employee (name,id,email)  {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+}
+
+
+Employee.prototype.setName = (name) => {
+    this.name = name;
+}
+
+Employee.prototype.setId = (id) => {
+    this.id = id;
+}
+
+Employee.prototype.setEmail = (email) => {
+    this.email = email;
+}
+
+Employee.getName = () => {
+    return Employee.prototype.name;
+}
+
+module.exports = Employee;
